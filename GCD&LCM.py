18a3eb338fa11856ea4f69d5dcc2271e8ad0a861
dhanasri <dhanasri24td0764@svcet.ac.in>
2026-07-31 03:17:13 -0700

    Update GCD&LCM.py

--- GCD&LCM.py
+++ GCD&LCM.py
@@ -1,10 +1,13 @@
 def gcd(a, b):
-    while b != 0:
+    while b:
         a, b = b, a % b
     return a
+
 def lcm(a, b):
     return (a * b) // gcd(a, b)
-a = 12
-b = 18
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
 print("GCD =", gcd(a, b))
 print("LCM =", lcm(a, b))
