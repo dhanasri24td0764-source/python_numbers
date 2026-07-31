b5c1e40bf95d76097b451ca69d8c76df5fd7bad2
dhanasri <dhanasri24td0764@svcet.ac.in>
2026-07-31 03:10:20 -0700

    Update fibonacci_series.py

--- fibonacci_series.py
+++ fibonacci_series.py
@@ -1,9 +1,8 @@
-n = int(input("Enter the number of terms: "))
-a = 0
-b = 1
-print("Fibonacci Series:")
+def fibonacci(n):
+    a = 0
+    b = 1
 for i in range(n):
-    print(a, end=" ")
-    c = a + b
-    a = b
-    b = c
+        print(a, end=" ")
+        a, b = b, a + b
+n = int(input("Enter the number of terms: "))
+fibonacci(n)
